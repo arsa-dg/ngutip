d8a9de97d5a154d283f4d92bcf0cb1f16a751591
arsa-dg <13519037@std.stei.itb.ac.id>
2021-09-13 07:34:03 +0700

    add tweet functionality

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,24 +1,12 @@
 import { useEffect, useState } from "react";
 import { fetchQuotes } from "../api/fetchQuotes";
 import { QuoteType } from "../interfaces/QuoteInterface";
+import { colors } from "../utils/constants/Colors";
 
 const Quote: React.FC = () => {
   const [quotes, setQuotes] = useState<Array<QuoteType>>([]);
   const [quote, setQuote] = useState<QuoteType>();
-  const colors = [
-    '#16a085',
-    '#27ae60',
-    '#2c3e50',
-    '#f39c12',
-    '#e74c3c',
-    '#9b59b6',
-    '#FB6964',
-    '#342224',
-    '#472E32',
-    '#BDBB99',
-    '#77B1A9',
-    '#73A857'
-  ];
+
   let i;
   var color = colors[Math.floor(Math.random()*colors.length)];
 
@@ -51,8 +39,21 @@ const Quote: React.FC = () => {
         <h1 style={{color: color}}>{`"`} {quote?.text} {`"`}</h1>
         <h2 style={{color: color}}>- {quote?.author ? quote?.author: "unknown"}</h2>
         <div className="buttons">
-          <button className="twt-btn">Tweet</button>
-          <button className="twt-next" onClick={handleClick}>hah</button>
+          <a
+            className="button"
+            href={`https://twitter.com/intent/tweet?hashtags=ngutip&text="${quote!.text}"%0D- ${quote!.author ? quote?.author: "unknown"}%0Dngutip.netlify.app%0D`}
+            target="_blank"
+            style={{backgroundColor: color}}
+          >
+            Tweet
+          </a>
+          <button 
+            className="button"
+            onClick={handleClick}
+            style={{backgroundColor: color}}
+          >
+            Next
+          </button>
         </div>
       </div>
       <style jsx>{`
@@ -67,6 +68,7 @@ const Quote: React.FC = () => {
           padding: 1em;
           background-color: white;
           box-shadow: 0 3px 10px rgb(0 0 0 / 0.5);
+          border-radius: 0.5em;
         }
         h1 {
           font-size: 1.25em;
@@ -78,9 +80,17 @@ const Quote: React.FC = () => {
         .buttons {
           position: absolute;
           bottom: 1em;
+          width: 94%;
+          display: flex;
+          justify-content: space-between;
         }
-        .twt-next {
-          
+        .button {
+          padding: 0.75em 1em;
+          color: white;
+          cursor: pointer;
+          border: none;
+          border-radius: 0.5em;
+          font-size: 1em;
         }
       `}</style>
     </div>
